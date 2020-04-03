a6cef8894e04470eafdddb74b95302ed242d4c7f
Dumidu Darshika Senanayake <52957290+daredumidu@users.noreply.github.com>
2020-04-03 22:43:36 +0530

    Add files via upload

---
+++ folder_list.py
@@ -0,0 +1,11 @@
+import os.path
+
+master_path = 'C:\\file_location'
+
+folder_name = os.listdir(master_path)           # list ltems in folder.
+
+#print (folder_name)
+
+with open('list.txt', 'w') as f:                # get the list to a txt file.
+    for item in folder_name:
+        f.write("%s\n" % item)
